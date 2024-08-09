cace0e6a88ec70e7bd15cfa8dd179f21e5b17df8
zaxwebs <zifidigital@gmail.com>
2024-08-09 22:38:02 +0530

    updated customers view

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,11 +1,39 @@
 <x-app-layout>
+	<x-slot name="header">
+		<h2 class="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+			{{ __('Customers') }}
+		</h2>
+	</x-slot>
+
 	<div class="py-12">
-		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-			<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-				<div class="p-6 text-gray-900 dark:text-gray-100">
-					{{ __("You're logged in!") }}
-				</div>
+		<div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+
+			<div class="space-y-3">
+				@foreach ($customers as $customer)
+					<div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+						<div class="grid gap-4 md:grid-cols-3">
+							<div class="flex flex-col flex-1 gap-2">
+								<div class="text-sm text-gray-500">Name</div>
+								<div>{{ $customer->name }}</div>
+							</div>
+							<div class="flex flex-col flex-1 gap-2">
+								<div class="text-sm text-gray-500">Email</div>
+								<div>{{ $customer->email }}</div>
+							</div>
+							<div class="flex flex-col flex-1 gap-2">
+								<div class="text-sm text-gray-500">Phone</div>
+								<div>{{ $customer->phone }}</div>
+							</div>
+						</div>
+					</div>
+				@endforeach
 			</div>
+
+
 		</div>
+
+
 	</div>
+
+
 </x-app-layout>
--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,28 +1,34 @@
 <x-app-layout>
+	<x-slot name="header">
+		<h2 class="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+			{{ __('Invoices') }}
+		</h2>
+	</x-slot>
+
 	<div class="py-12">
-		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+		<div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
 
 			<div class="space-y-3">
 				@foreach ($invoices as $invoice)
-					<div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+					<div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
 						<div class="grid gap-4 md:grid-cols-5">
-							<div class="flex gap-2 flex-col flex-1">
+							<div class="flex flex-col flex-1 gap-2">
 								<div class="text-sm text-gray-500">Number</div>
 								<div>{{ strtoupper($invoice->invoice_number) }}</div>
 							</div>
-							<div class="flex gap-2 flex-col flex-1">
+							<div class="flex flex-col flex-1 gap-2">
 								<div class="text-sm text-gray-500">Billed To</div>
 								<div>{{ $invoice->customer->name }}</div>
 							</div>
-							<div class="flex gap-2 flex-col flex-1">
+							<div class="flex flex-col flex-1 gap-2">
 								<div class="text-sm text-gray-500">Due Date</div>
 								<div>{{ $invoice->due_date }}</div>
 							</div>
-							<div class="flex gap-2 flex-col flex-1">
+							<div class="flex flex-col flex-1 gap-2">
 								<div class="text-sm text-gray-500">Total</div>
-								<div class="font-bold">$1,200</div>
+								<div class="text-lg font-bold md:text-base">$1,200</div>
 							</div>
-							<div class="flex gap-2 flex-col flex-1 md:items-end">
+							<div class="flex flex-col flex-1 gap-2 md:items-end">
 								<div class="text-sm text-gray-500">Status</div>
 								<div>{{ $invoice->status }}</div>
 							</div>
