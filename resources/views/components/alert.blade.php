39f4c807aac84547bb45489e2bde07aeb70b6557
zaxwebs <zifidigital@gmail.com>
2024-08-12 22:26:21 +0530

    added alert levels

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,10 +1,9 @@
 @props(['id' => 'alert'])
 
 @if(session($id))
-	<div class="p-4 mt-2 text-sm text-teal-800 border border-teal-500 rounded-lg bg-teal-50 dark:bg-teal-800/10 dark:border-teal-900 dark:text-teal-500"
-		role="alert" tabindex="-1" aria-labelledby="hs-soft-color-success-label">
+	<div {{ $attributes->class(['text-teal-800 border-teal-500 bg-teal-50' => session($id)['type'] === 'success','text-blue-800 border-blue-500 bg-blue-50' => session($id)['type'] === 'info', 'text-red-800 border-red-500 bg-red-50' => session($id)['type'] === 'danger'])->merge(['class' => 'p-4 mt-2 text-sm  border  rounded-lg ']) }} role="alert" tabindex="-1">
 		<!-- <span id="hs-soft-color-success-label" class="font-bold">Success</span> alert! You should check in on some of those
-																fields below. -->
+																					fields below. -->
 		{{ session($id)['message'] }}
 	</div>
 @endif
