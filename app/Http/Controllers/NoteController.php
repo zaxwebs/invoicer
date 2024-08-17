5854d00c6bfaf3cf21ec6b4e9d6746d13a27b278
zaxwebs <zifidigital@gmail.com>
2024-08-17 22:15:33 +0530

    temporary note handling

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class NoteController extends Controller
 {
 	public function store(Request $request)
 	{
-		$validated = $request->validate([
-			'invoice_id' => 'required|exists:invoices,id',
-			'content' => 'required|string',
-		]);
+		try {
+			$validated = $request->validate([
+				'invoice_id' => 'required|exists:invoices,id',
+				'content' => 'required|string|min:2',
+			]);
+		} catch (ValidationException $e) {
+			return redirect()->back()->with('alert', alertify('Something went wrong!', 'danger'));
+		}
 
 		Note::create($validated);
 
