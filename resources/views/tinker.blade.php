d20b8d32f0f206ca02b6783b68c6b5b10b095234
zaxwebs <zifidigital@gmail.com>
2024-08-21 21:36:39 +0530

    WIP modal + livewire

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 	<div class="p-4">
-		<x-modal title="Create Customer">
-			<livewire:create-customer modal={{ true }} />
+		<x-modal id="modal" title="Create Customer">
+			<livewire:create-customer modal="modal" />
 		</x-modal>
 	</div>
 </x-app-layout>
