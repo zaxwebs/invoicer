de16f358407ee922f2e390e24da2d62133a991a7
zaxwebs <zifidigital@gmail.com>
2024-08-23 00:11:26 +0530

    WIP modal

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -12,7 +12,7 @@
 	<x-danger-button x-data=""
 		x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">{{ __('Delete Account') }}</x-danger-button>
 
-	<x-simple-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+	<x-native-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
 		<form method="post" action="{{ route('profile.destroy') }}" class="p-6">
 			@csrf
 			@method('delete')
@@ -44,5 +44,5 @@
 				</x-danger-button>
 			</div>
 		</form>
-	</x-simple-modal>
+	</x-native-modal>
 </section>
