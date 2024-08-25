7c63bff4ca925eefa471709c4c8c640d9bacf48d
zaxwebs <zifidigital@gmail.com>
2024-08-25 09:53:54 +0530

    WIP

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,5 +1,10 @@
 @props(['customer'])
 
-<img class="inline-block object-cover border border-gray-100 rounded-full size-16"
-	src="{{ $customer->image ? asset('storage/' . $customer->image) : asset('images/user-avatar.svg') }}"
+@php
+	$imageUrl = $customer->image
+		? asset('storage/' . $customer->image)
+		: asset('images/user-avatar.svg');
+@endphp
+
+<img class="inline-block object-cover border border-gray-100 rounded-full size-16" src="{{ $imageUrl }}"
 	alt="{{ $customer->name }}" />
