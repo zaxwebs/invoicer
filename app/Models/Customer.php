f4c1c8011a3ec4e80af4cb8bd53ae8d5bf98d613
zaxwebs <zifidigital@gmail.com>
2024-08-25 11:03:35 +0530

    image accessor for customers

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Invoice;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Customer extends Model
@@ -11,6 +12,13 @@ class Customer extends Model
 	use HasFactory;
 
 	protected $guarded = [];
+
+	protected function image(): Attribute
+	{
+		return Attribute::make(
+			get: fn(string|null $value) => $value ? 'storage/' . $value : 'images/user-avatar.svg',
+		);
+	}
 
 	public function invoices()
 	{
--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,10 +1,4 @@
 @props(['customer'])
 
-@php
-	$imageUrl = $customer->image
-		? asset('storage/' . $customer->image)
-		: asset('images/user-avatar.svg');
-@endphp
-
-<img class="inline-block object-cover border border-gray-100 rounded-xl size-20" src="{{ $imageUrl }}"
+<img class="inline-block object-cover border border-gray-100 rounded-xl size-20" src="{{ asset($customer->image) }}"
 	alt="{{ $customer->name }}" />
