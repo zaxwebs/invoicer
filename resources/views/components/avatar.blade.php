72fdb9fd7f399e0e70e8bcf11d08a2b48a8deb62
zaxwebs <zifidigital@gmail.com>
2024-08-26 13:59:44 +0530

    WIP

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,7 +1,7 @@
 @props(['customer'])
 
 @php
-	$classes = 'inline-block object-cover border border-gray-100 rounded-xl size-20 bg-white';
+	$classes = 'inline-block object-cover border border-gray-100 rounded-xl size-20 bg-slate-100';
 @endphp
 
 <img {{ $attributes->merge(['class' => $classes]) }} src="{{ asset($customer->image) }}" alt="{{ $customer->name }}" />
