59a72cac1edc9d4a0a900ed4a75574e4e693e90c
zaxwebs <zifidigital@gmail.com>
2024-08-26 09:41:46 +0530

    updated badge

--- resources/views/components/status-badge.blade.php
+++ resources/views/components/status-badge.blade.php
@@ -13,7 +13,9 @@
 	@if ($status === InvoiceStatus::PAID)
 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
 			stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="shrink-0 size-4">
-			<path d="M20 6 9 17l-5-5" />
+			<path
+				d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+			<path d="m9 12 2 2 4-4" />
 		</svg>
 	@endif
 	@if ($status === InvoiceStatus::PARTIALLY_PAID)
