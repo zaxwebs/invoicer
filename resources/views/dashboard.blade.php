2402a11181d8ecceddf883d24df5c1e9d6ed2b98
zaxwebs <zifidigital@gmail.com>
2024-08-27 06:20:26 +0530

    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -176,27 +176,10 @@
 							<p class="text-xs tracking-wide text-gray-500 uppercase dark:text-neutral-500">
 								Active Invoices Total
 							</p>
-							<div class="flex items-center gap-2 mt-1">
+							<div class="mt-1">
 								<h3 class="text-xl font-medium text-gray-800 sm:text-2xl dark:text-neutral-200">
 									{{ Illuminate\Support\Number::abbreviate($activeInvoicesTotal) }}
 								</h3>
-								<div class="hs-tooltip">
-									<div class="hs-tooltip-toggle">
-										<svg class="text-gray-500 shrink-0 size-4 dark:text-neutral-500"
-											xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-											viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-											stroke-linecap="round" stroke-linejoin="round">
-											<circle cx="12" cy="12" r="10" />
-											<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-											<path d="M12 17h.01" />
-										</svg>
-										<span
-											class="absolute z-10 invisible inline-block px-2 py-1 text-xs font-medium text-white transition-opacity bg-gray-900 rounded shadow-sm opacity-0 hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible dark:bg-neutral-700"
-											role="tooltip">
-											<x-money :value="$activeInvoicesTotal" />
-										</span>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
@@ -222,27 +205,10 @@
 							<p class="text-xs tracking-wide text-gray-500 uppercase dark:text-neutral-500">
 								Paid Invoices Total
 							</p>
-							<div class="flex items-center gap-2 mt-1">
+							<div class="mt-1">
 								<h3 class="text-xl font-medium text-gray-800 sm:text-2xl dark:text-neutral-200">
 									{{ Illuminate\Support\Number::abbreviate($paidInvoicesTotal) }}
 								</h3>
-								<div class="hs-tooltip">
-									<div class="hs-tooltip-toggle">
-										<svg class="text-gray-500 shrink-0 size-4 dark:text-neutral-500"
-											xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-											viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-											stroke-linecap="round" stroke-linejoin="round">
-											<circle cx="12" cy="12" r="10" />
-											<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-											<path d="M12 17h.01" />
-										</svg>
-										<span
-											class="absolute z-10 invisible inline-block px-2 py-1 text-xs font-medium text-white transition-opacity bg-gray-900 rounded shadow-sm opacity-0 hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible dark:bg-neutral-700"
-											role="tooltip">
-											<x-money :value="$paidInvoicesTotal" />
-										</span>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
