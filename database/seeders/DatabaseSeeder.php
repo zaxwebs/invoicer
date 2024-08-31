ef445f3771d44455867f2e5b6af509f9d25a9530
zaxwebs <zifidigital@gmail.com>
2024-08-31 23:45:22 +0530

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Note;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Invoice;
 use App\Models\Settings;
 use App\Models\Customer;
@@ -17,29 +16,34 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run(): void
 	{
-		// User::factory(10)->create();
-
 		$user = User::factory()->create([
 			'name' => 'Test User',
 			'email' => 'test@example.com',
 		]);
 
-		$settings = Settings::factory()->create(['user_id' => $user->id]);
+		Settings::factory()->create(['user_id' => $user->id]);
 
+		// Create customers
 		$customers = Customer::factory()
 			->count(20)
 			->create(['user_id' => $user->id]);
 
-		Invoice::factory()
-			->count(200)
-			->make()
-			->each(function ($invoice) use ($customers) {
-				$invoice->customer_id = $customers->random()->id;
-				$invoice->save();
-				$numberOfNotes = rand(0, 3); // Random number between 0 and 3
-				Note::factory()
-					->count($numberOfNotes)
-					->create(['invoice_id' => $invoice->id]); // Create notes for this invoice
-			});
+		// Create invoices
+		$invoices = Invoice::factory()
+			->count(400)
+			->make(['customer_id' => $customers->random()->id]);
+
+		// Assign invoices to customers randomly
+		foreach ($invoices as $invoice) {
+			$customer = $customers->random();
+			$invoice->customer_id = $customer->id;
+			$invoice->save();
+
+			// Create notes for each invoice
+			$numberOfNotes = rand(0, 3);
+			Note::factory()
+				->count($numberOfNotes)
+				->create(['invoice_id' => $invoice->id]);
+		}
 	}
 }
