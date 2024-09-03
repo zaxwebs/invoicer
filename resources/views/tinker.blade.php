4db540561d17c7fd3bab3b1f95a182cd964530b2
zaxwebs <zifidigital@gmail.com>
2024-09-03 19:19:56 +0530

    WIP

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
 	<div class="p-4 space-y-6">
 		<x-modal id="create-customer-modal" title="Create Customer">
-			<livewire:create-customer modal="create-customer-modal" />
 		</x-modal>
-
 	</div>
 </x-app-layout>
