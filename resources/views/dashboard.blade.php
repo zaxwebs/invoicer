2a058760456487a396f4c525d61a1860d5a0b928
zaxwebs <zifidigital@gmail.com>
2024-09-05 11:03:16 +0530

    updated dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,10 @@
 <x-app-layout>
-	<x-slot name="header">
-		<h2 class="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
-			{{ __('Dashboard') }}
-		</h2>
-	</x-slot>
-
-	<div class="py-12">
+	<section style="background-image: url('{{ asset('images/gradient.jpg') }}');" class="py-20 bg-slate-900">
 		<div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-
 			<div class="grid grid-cols-2 gap-4 lg:grid-cols-4">
 				<div
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-blue-100 rounded-lg dark:bg-neutral-800">
 
@@ -60,7 +53,7 @@
 
 				<x-link :href="route('invoices.index', ['status' => 'paid'])"
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-teal-100 rounded-lg dark:bg-neutral-800">
 
@@ -88,7 +81,7 @@
 
 				<div
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-blue-100 rounded-lg dark:bg-neutral-800">
 
@@ -115,7 +108,7 @@
 
 				<x-link :href="route('invoices.index', ['status' => 'overdue'])"
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-fuchsia-100 rounded-lg dark:bg-neutral-800">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -143,7 +136,7 @@
 
 				<div
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-neutral-800">
 							<svg class="text-gray-600 shrink-0 size-6 dark:text-neutral-400"
@@ -172,7 +165,7 @@
 
 				<div
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-neutral-800">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -198,7 +191,7 @@
 
 				<div
 					class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-					<div class="flex flex-col gap-4 p-4 md:flex-row md:p-6">
+					<div class="flex flex-col gap-4 p-4 md:p-6">
 						<div
 							class="shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-neutral-800">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -223,7 +216,11 @@
 				</div>
 
 			</div>
+		</div>
+	</section>
 
+	<div class="py-12">
+		<div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
 			<div class="flex items-center justify-between">
 				<h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
 					{{ __('Invoices') }}
